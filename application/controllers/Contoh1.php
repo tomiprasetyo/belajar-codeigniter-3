e5e7dfbd5c324ddf1c59406bf560389998d45a9e
tomiprasetyo <tomi.prasetyo95@gmail.com>
2021-09-18 14:24:07 +0700

    Menambahkan file Contoh1.php didalam folder controller

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+class Contoh1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "<h1></h1>";
+        echo "Nama saya Tomi Prasetyo
+        saya tinggal di Cilincing
+        saat ini saya sedang kuliah reguler malam di UBSI Kramat Raya";
+    }
+}
