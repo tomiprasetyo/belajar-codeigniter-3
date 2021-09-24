316be0d5c41f1b60d6a26fca56a2f2ef95a2270d
tomiprasetyo <tomi.prasetyo95@gmail.com>
2021-09-25 05:44:28 +0700

    P-3

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Tomi</li>
+        <li>Nama Belakang : Prasetyo</li>
+    </ul>
+
+    <br>
+
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Rekreasi Cilincing Rt. 03 Rw 04</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="Square">
+        <li>Bersepeda</li>
+        <li>Berenang</li>
+    </ul>
+</section>
